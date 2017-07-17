499f0d686ecc42271af93fab442483b064987dcf
Kishu Agarwal <kishu@hackerearth.com>
2017-07-17 21:54:59 +0530

    add base jump timestamp service

---
+++ timestamp.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+
+app.get('/:time', function(req, res) {
+    var result = {
+        'unix': null,
+        'natural': null
+    }
+    try {
+        var decoded = decodeURI(req.params.time);
+        var timestamp = parseInt(decoded);
+        var date;
+        if (timestamp == timestamp) {
+            date = new Date(timestamp);
+        } else {
+            date = new Date(decoded);
+        }
+        result.unix = date.getTime();
+        result.natural = date.toDateString();
+    }
+    catch(err) {
+    }
+    res.json(result);
+});
+
+
+app.listen(4000);
